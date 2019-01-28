355395e74dc5330c9783fc8ba41519dda4e34c50
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-01-27 21:30:59 -0700

    Updates

---
+++ precache-manifest.04e8c37521ae1a814dcb82d18860e02d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "30929b88e9d3288f5c9d",
+    "url": "/clicky-game/static/js/main.30929b88.chunk.js"
+  },
+  {
+    "revision": "7644a9751e730a17c6a5",
+    "url": "/clicky-game/static/js/1.7644a975.chunk.js"
+  },
+  {
+    "revision": "30929b88e9d3288f5c9d",
+    "url": "/clicky-game/static/css/main.5bff3ae8.chunk.css"
+  },
+  {
+    "revision": "68e105fc46aa842b70b84561d2849010",
+    "url": "/clicky-game/index.html"
+  }
+];
