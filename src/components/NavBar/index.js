ddab2de06d2054f7d19dd99b3c14c52a2d04867f
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-01-27 22:18:51 -0700

    updated navbar title - updated readme

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 const NavBar = props => (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1" to="/">Clicky Game</span>
+        <span className="navbar-brand mb-0 h1" to="/">Colorblox</span>
         <ul className="navbar-nav">
             <li>Score: {props.score}</li>
             <li>Top Score: {props.topScore}</li>
